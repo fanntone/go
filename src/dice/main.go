9efec41c1ff9dd94f52f06e0af98c05cfa4dd1ce
Weihung <weihung@inno-orz.com>
2019-11-15 11:43:50 +0800

    Add CORS

--- src/dice/main.go
+++ src/dice/main.go
@@ -3,18 +3,25 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/rs/cors"
 )
 
-
 func main() {
-	http.HandleFunc("/testrand", testrand)
-	http.HandleFunc("/PlayEtheRoll", PlayEtheRoll)
-	http.HandleFunc("/PlayRollADice", PlayRollADice)
-	http.HandleFunc("/PlayTwoDice", PlayTwoDice)
-	http.HandleFunc("/PlayCoinFilp", PlayCoinFilp)
- 
-	err := http.ListenAndServe(":8080", nil)
-	if (err != nil) {
-		log.Fatal(err)
+
+	port := "8080"
+	if v := os.Getenv("PORT"); len(v) > 0 {
+		port = v
 	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/testrand", testrand)
+	mux.HandleFunc("/PlayEtheRoll", PlayEtheRoll)
+	mux.HandleFunc("/PlayRollADice", PlayRollADice)
+	mux.HandleFunc("/PlayTwoDice", PlayTwoDice)
+	mux.HandleFunc("/PlayCoinFilp", PlayCoinFilp)
+
+	handler := cors.Default().Handler(mux)
+	log.Fatal(http.ListenAndServe(":"+port, handler))
 }
